bb5bd1309b7839511d32210286ce68630e59f282
Eileen Li <eileenli@eileens-mbp.wifi.local.cmu.edu>
2022-11-07 15:54:32 -0500

    small changes to run script

--- run_scripts/run_dqn.py
+++ run_scripts/run_dqn.py
@@ -16,7 +16,7 @@ from stable_baselines3.common.env_checker import check_env
 MIDI_SAVEDIR = "./samples/dqn/"
 
 
-def create_dqn_model(env, args, log_dir):
+def create_dqn_model(env, args, log_dir=None):
     model = DQN("MlpPolicy", env,
                 learning_rate=args.learning_rate,
                 learning_starts=args.learning_starts,
@@ -30,7 +30,8 @@ def create_dqn_model(env, args, log_dir):
     return model
 
 
-def train_loop(model, timesteps, log_interval, progress_bar=True):
+def train_loop(model, timesteps, log_interval, test_interval, progress_bar=True):
+    #TODO: add EvalCallback to use test_interval
     model.learn(total_timesteps=timesteps, log_interval=log_interval, progress_bar=progress_bar)
 
 
@@ -51,47 +52,59 @@ def sample_test_trajectory(model, env, num_trajectories=20):
         if terminated:
             env.render()
             env.reset()
+        # if using model.get_env()
         # final_trajectory = env.buf_infos[0]['terminal_observation']
         # RoboNotesEnv.save_midi(final_trajectory, midi_savedir)
 
 
-def run_dqn(args, log_dir):
+def run_dqn(args, log_dir=None):
     midi_savedir = MIDI_SAVEDIR if args.save_midi else None
     env = RoboNotesEnv(max_trajectory_len=args.max_trajectory_len, midi_savedir=midi_savedir)
     check_env(env)
-    model = create_dqn_model(env, args, log_dir)
+
+    model = create_dqn_model(env, args, log_dir=log_dir)
 
     if args.load_model_path:
-        print(f"Loading model from path: {args.load_model_path}")
+        print(f"Skipping training. Loading model from path: {args.load_model_path}")
         model = model.load(args.load_model_path)
     else:
-        train_loop(model, timesteps=args.total_timesteps, log_interval=args.log_interval)
+        print(f"Start training.")
+        train_loop(
+            model,
+            timesteps=args.total_timesteps,
+            test_interval=args.test_interval,
+            log_interval=args.log_interval
+        )
         if args.save_model:
             model.save(f"{log_dir}/dqn")
 
+    print("Finished. Sampling test trajectories.")
     sample_test_trajectory(model, env)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--exp_name", type=str, required=True, help="name the experiment, for logging")
+    parser.add_argument("--exp_name", type=str, required=True, help="name the experiment, for log dir")
+
     # train parameters
-    parser.add_argument("--total_timesteps", type=int, default=500000, required=False,
+    parser.add_argument("--total_timesteps", type=int, default=10000000, required=False,
                         help="number of timesteps (env steps) to train")
-    parser.add_argument("--log_interval", type=int, default=20, required=False,
+    parser.add_argument("--log_interval", type=int, default=100, required=False,
                         help="number of timesteps before logging")
+    parser.add_argument("--test_interval", type=int, default=100000, required=False,
+                        help="number of timesteps before testing")
     # model parameters
     parser.add_argument("--learning_rate", type=float, default=0.0001, required=False,
                         help="learning rate")
     parser.add_argument("--learning_starts", type=int, default=100000, required=False,
                         help="how many steps of the model to collect transitions for before learning starts")
-    parser.add_argument("--batch_size", type=int, default=64, required=False,
+    parser.add_argument("--batch_size", type=int, default=32, required=False,
                         help="minibatch size of each GD step")
-    parser.add_argument("--train_freq", type=int, default=20, required=False,
+    parser.add_argument("--train_freq", type=int, default=4, required=False,
                         help="update model every X steps")
     parser.add_argument("--gradient_steps", type=int, default=1, required=False,
                         help="how many gradient steps to do after each rollout")
-    parser.add_argument("--target_update_interval", type=int, default=10, required=False,
+    parser.add_argument("--target_update_interval", type=int, default=10000, required=False,
                         help="update target network every X env steps")
 
     # env parameters
@@ -103,7 +116,7 @@ if __name__ == "__main__":
     parser.add_argument("--load_model_path", type=str,
                         help="If provided, path of the dqn model to load.", required=False)
     args = parser.parse_args()
-    print("Args: ", args)
+    print("\n\n\nARGS: ", args, "\n\n\n")
 
     data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../run_logs')
 
@@ -117,4 +130,4 @@ if __name__ == "__main__":
 
     print("\n\n\nLOGGING TO: ", logdir, "\n\n\n")
 
-    run_dqn(args, logdir)
+    run_dqn(args, log_dir=logdir)
